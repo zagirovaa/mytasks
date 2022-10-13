8f4bb1b13e92acff09b4734271dec5036a3c9666
Abdul Zagirov <zagirovaa@netcon.pro>
2022-10-13 11:22:39 +0300

    Moved to type module.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
