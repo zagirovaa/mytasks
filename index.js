bf1b78aa65deb76e0cd96f92971943693c70eaf4
Abdul Zagirov <zagirovaa@netcon.pro>
2022-10-21 13:37:03 +0300

    Automized vite building process.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static("dist"));
 app.use("/", (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 module.exports = app;
